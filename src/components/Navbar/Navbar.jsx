71718126fd2f3b73d572b95812a62b404274dff4
anbuvagai <anbuvagai@gmail.com>
2024-08-28 17:15:29 +0530

    initial Files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <a href="/" className="logo">
+        logo
+      </a>
+
+      <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Services</a>
+        <a href="/">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
